0a951a25c95a5e7810cf0dd07caeb82596b8c6bc
Alex De Meo <alexdm9572@gmail.com>
2023-02-10 17:41:00 +0000

    MathOps est fini

---
+++ MathOps.swift
@@ -0,0 +1,29 @@
+//
+//  MathOps.swift
+//
+//  Created by Alex De Meo
+//  Created on 2023/02/09
+//  Version 1.0
+//  Copyright (c) 2023 Alex De Meo. All rights reserved.
+//
+//  Shows many different math operators usable in swift
+// Importing packages.
+import Swift
+import Foundation
+// Addition with Ints.
+print("8 + 7 = \(8 + 7)")
+// Subtraction with Ints.
+print("10 - 12 = \(10 - 12)")
+// Multiplication with Ints.
+print("8 * 5 = \(8 * 5)")
+// Division with Ints.
+print("10 / 5 = \(10 / 5)")
+// Division with floats.
+print("22 / 7 = \(Float(22) / Float(7))")
+// Division with doubles.
+print("22 / 7 = \(Double(22) / Double(7))")
+// A number to the power of 2.
+print("3 ^ 2 = \(pow(3,2))")
+// A number to the power of 3
+print("3 ^ 3 = \(pow(3, 3))")
+print("√16 = \(sqrt(16))")  
